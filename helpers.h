e9c5b66379017ebd7e06932e4580ff31f8c35184
Azamat Tskhovrebov <AzamatTheBest.cs.py@gmail.com>
2020-10-29 22:30:23 +0300

    Create helpers.h

---
+++ helpers.h
@@ -0,0 +1,13 @@
+
+
+#define NUMBER '0'
+#define MAX_LEN 128
+#define BUFFER_SIZE 128
+
+typedef char  int8;
+typedef short int16;
+typedef int   int32;
+
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
